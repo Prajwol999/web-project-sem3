788026f55d52f7c998bcf87f5e1e61d762a2f05a
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2024-08-19 00:26:45 +0545

    security added and some corrections

--- src/main/java/com/example/prajwol/config/PasswordEncoderUtil.java
+++ src/main/java/com/example/prajwol/config/PasswordEncoderUtil.java
@@ -1,12 +1,12 @@
-//package com.example.prajwol.config;
-//
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//public class PasswordEncoderUtil {
-//    private static final BCryptPasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
-//
-//    public static BCryptPasswordEncoder getInstance() {
-//        return PASSWORD_ENCODER;
-//    }
-//
-//}
+package com.example.prajwol.config;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderUtil {
+    private static final BCryptPasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
+
+    public static BCryptPasswordEncoder getInstance() {
+        return PASSWORD_ENCODER;
+    }
+
+}
--- src/main/java/com/example/prajwol/config/WebConfig.java
+++ src/main/java/com/example/prajwol/config/WebConfig.java
@@ -1,17 +1,17 @@
-//package com.example.prajwol.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedHeaders("*").allowedMethods("GET","DELETE","PATCH","PUT","POST");
-//
-//    }
-//}
+package com.example.prajwol.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("*").allowedMethods("GET","DELETE","PATCH","PUT","POST");
+
+    }
+}
